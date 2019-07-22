452138c00e6db15a97764a85fa70b9ff8c3e104c
Lloyd Risper <lloydrisper@me.com>
2019-07-22 01:43:00 -0400

    Added Audio

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    AudioSource _audioSource;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddRelativeForce(Vector3.up);
+
+            if (!_audioSource.isPlaying) // so it don't layer
+            {
+                _audioSource.Play();
+            }
+        }
+        else
+        {
+            _audioSource.Stop();
+        }
+    }
+
+
+    private void Rotate()
+    {
+        _rigidbody.freezeRotation = true; // take manual control
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        _rigidbody.freezeRotation = false;
+    }
+}
+
+
+
+
+
+
+
+
+
